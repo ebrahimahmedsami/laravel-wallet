3fe4d900d2468a0767c927b37cb9ebef073027f3
EbrahimSami <112947751+EbrahimSami@users.noreply.github.com>
2023-10-21 21:25:42 +0300

    feat(migration): add wallet table migration

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # laravel-wallet
 This package for using wallet with user
+
+# Create Wallet Model
+    Columns : ['type', 'transaction_reasons',
+                    'transaction_type','current', 'steps',
+                    'balance', 'data']
+
+    ------------------------------------------
+
+    Relation : 
+        public function wallettable(): MorphTo
+        {
+            return $this->morphTo();
+        }
+
---
+++ src/Support/WalletServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Himasami\LaravelWallet\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+class WalletServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        // Register any bindings or dependencies here
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/database/migrations/' => database_path('migrations')
+        ], 'laravel-wallet-migrations');
+    }
+
+}
